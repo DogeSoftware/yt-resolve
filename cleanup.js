6fe5a283d05a418fa0d574c745d20f5b1ef42a71
LCD <37777903+PhoenixDiscord@users.noreply.github.com>
2020-05-21 16:27:48 +0200

    Update cleanup.js

--- cleanup.js
+++ cleanup.js
@@ -4,6 +4,6 @@ console.log(chalk.yellow('Cleaning up..'));
 
 fs.writeFileSync('./progress.txt', '0', 'utf8');
 fs.writeFileSync('./queries.txt', '', 'utf8');
-fs.writeFileSync('./results.txt', '', 'utf8');
+fs.writeFileSync('./result.txt', '', 'utf8');
 
 console.log(chalk.green('Done!'));
