8b224846ee3f061561799188abf10d9d0fd6ecd8
Quin <quin@cheesits456.dev>
2020-05-21 08:12:05 -0700

    Update index.js
    Minified some code, unified the formatting, added a few comments

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
 // Thanks Stackoverflow!
-const search = require('yt-search');
-const fs = require('fs');
-const progress = parseInt(fs.readFileSync('./progress.txt', 'utf8'));
-const config = require('./config.json');
-const resultfile = fs.createWriteStream('./result.txt', { flags: 'a' });
-const chalk = require('chalk');
 
+// Require Modules
+const chalk = require('chalk'),
+	fs = require('fs'),
+	search = require('yt-search');
+
+// Load Configuration
+const	config = require('./config.json'),
+	progress = parseInt(fs.readFileSync('./progress.txt', 'utf8')),
+	resultfile = fs.createWriteStream('./result.txt', { flags: 'a' });
+	
+// Define Functions
 function sleep(ms) {
-	return new Promise((resolve) => {
-		setTimeout(resolve, ms);
-	});
+	return new Promise(res => setTimeout(res, ms));
 }
 
-function get_line(filename, line_no, callback) {
-	fs.readFile(filename, function(err, data) {
+function getLine(filename, lineNum, callback) {
+	fs.readFile(filename, (err, data) => {
 		if (err) throw err;
-
 		// Data is a buffer that we need to convert to a string
 		// Improvement: loop over the buffer and stop when the line is reached
 		const lines = data.toString('utf-8').split('\n');
-
-		if(+line_no > lines.length) {
-			return callback('File end reached without finding line', null);
-		}
-
-		callback(null, lines[+line_no]);
+		if(+lineNum > lines.length) return callback('File end reached without finding line', null);
+		callback(null, lines[+lineNum]);
 	});
 }
 
-get_line(config.file, progress, function(err, line) {
-	console.log(chalk.yellow('Searching: ' + line));
-	if(line == undefined) return console.log(chalk.yellow(`Task finished, or no search queries in ${config.file}!`));
-	search(line, function(err, r) {
-		if(r) {
-			const videos = r.videos;
+// Call Functions
+getLine(config.file, progress, (err, line) => {
+	console.log(chalk.yellow(`Searching: ${line}`));
+	if (line == undefined) return console.log(chalk.yellow(`Task finished, or no search queries in ${config.file}!`));
+	search(line, (err, res) => {
+		if (res) {
+			const videos = res.videos;
 			console.log(chalk.green(`Found video: ${videos[0].title}`));
 			// saves the current line
 			fs.writeFileSync('./progress.txt', `${progress + 1}`, 'utf8');
 			// saves the output url
 			resultfile.write(`${videos[0].url}\n`);
 			resultfile.end();
-		}
-		else {
-			return console.log(chalk.red('Could not find video! Ratelimit? Retrying..'));
-		}
+		} else return console.log(chalk.red('Could not find video! Ratelimit? Retrying..'));
 	});
 	sleep(250);
 });
